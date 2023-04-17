fc6c709c3e2b5ea2aba090c7428f2631d71a982b
Muhammadqodir <m.qodir777@gmail.com>
2023-04-18 00:58:08 +0300

    Configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:suxoy_zakon/pages/cart_page.dart';
 import 'package:suxoy_zakon/theme.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
